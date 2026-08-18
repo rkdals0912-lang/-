d1bb7c997c197b38ad8dae718d239e48cb771ed3
rkdals0912-lang <rkdals0912@gmail.com>
2026-08-19 06:29:28 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+// KSA 라벨 웹앱 설정
+// Supabase 프로젝트의 Project URL / publishable key를 입력하세요.
+// 입력하지 않으면 브라우저 localStorage 모드로 동작합니다.
+window.KSA_CONFIG = {
+  SUPABASE_URL: "https://mbcymglgukrlleguopug.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_2K8t4TrD5fLg8Kgzp7VjIQ_ZxOnJ8t8"
+};
