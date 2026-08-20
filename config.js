9884bb12116916d9771dab44e3cba89c59b72a1b
rkdals0912-lang <rkdals0912@gmail.com>
2026-08-20 21:43:34 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.KSA_CONFIG = {
   SUPABASE_URL: "https://mbcymglgukrlleguopug.supabase.co",
-  SUPABASE_ANON_KEY: "sb_publishable_2K8t4TrD5fLg8Kgzp7VjIQ_ZxOnJ8t8"
+  SUPABASE_PUBLISHABLE_KEY: "sb_publishable_..."
 };
